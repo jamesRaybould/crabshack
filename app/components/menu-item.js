b0c8adcd6bb61eb4322b5ad803bb327713eecde3
James Raybould <james.raybould@gmail.com>
2015-02-06 23:15:36 +0000

    Re-worked the menu hiding to use `bind-attr` rather than the global jQuery

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	click: function(evt){
-			var target = $(evt.currentTarget);
-			
-			target.find(".description").toggle("hidden");
+			this.set("isHidden", !this.get("isHidden"));
+		},
 
-		}
+		isHidden: function(){
+			return true;
+		}.property()
 });
