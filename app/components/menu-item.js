301fb055211f7c86090a8056274d8e2489141d8d
James Raybould <james.raybould@gmail.com>
2015-02-10 22:05:31 +0000

    Don't invert the property manually use `toggleProperty` instead

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	click: function(){
-		this.set("isHidden", !this.get("isHidden"));
+		this.toggleProperty("isHidden");
 	},
 	//Default out the `isHidden` value to true so we *don't* show the description by default
 	isHidden: function(){
