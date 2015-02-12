132033aae2e45e640cf721de624a4ec60d622dd8
James Raybould <james.raybould@gmail.com>
2015-02-12 23:45:30 +0000

    Added a working implementation of subtotal and added a `Clear All` button The order view autohides if there are no items currently in the order

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -7,6 +7,21 @@ export default Ember.ArrayController.extend({
 			var order = this.store.createRecord('order', {tableNumber: 1});
 			order.get('orderItems').addObjects(orderItems);
 			order.save();
+		},
+		
+		clearAll:function(){
+			this.clear();
 		}
 	},
+
+	subTotal: function(){
+		return this.get("model").reduce(function(previousValue, item){
+			var price = Number(item.get("price"));
+			return previousValue + price;
+		},0).toFixed(2);
+	}.property('@each.price'),
+
+	hasItems: function(){
+		return this.get('model').length > 0;
+	}.property('@each.item')
 });
