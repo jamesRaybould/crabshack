038149993e3ab84e7b8aa04e19c2a6651fcb7d95
James Raybould <james.raybould@gmail.com>
2015-02-15 00:33:55 +0000

    Fixed the annoying defect where clicking order would also toggle the hidden description

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -6,8 +6,10 @@ export default Ember.Component.extend({
 			this.sendAction('order', this.get("item"));
 		}
 	},
-	click: function(){
-		this.toggleProperty("isHidden");
+	click: function(evt){
+		if (evt.target.nodeName != "BUTTON") {
+			this.toggleProperty("isHidden");
+		};
 	},
 	//Default out the `isHidden` value to true so we *don't* show the description by default
 	isHidden: function(){
