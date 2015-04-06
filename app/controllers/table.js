aa590cc161d8afbb8c11910afd12f733ec093245
James Raybould <james.raybould@gmail.com>
2015-04-06 23:44:10 +0100

    Add `hasItems` for the table controller Added elements.less for reasons

--- app/controllers/table.js
+++ app/controllers/table.js
@@ -6,5 +6,12 @@ export default Ember.Controller.extend({
         setInterval(function() {
             _this.store.fetchById('table', 1);
         }, 30000);
-    }
+    },
+    hasItems: function() {
+
+        if (this.get("model").get('orderItems') != undefined) {
+            return this.get("model").get('orderItems').length > 0;
+        };
+        return false;
+    }.property('model.@each.orderItem'),
 });
